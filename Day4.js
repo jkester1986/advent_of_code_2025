abe89cd230078db9ad2a53c2f6c5e947a593dc0c
Julia Kester <jkester@acvauctions.com>
2025-12-04 13:22:41 -0500

    Day 4 P2 solved

--- Day4.js
+++ Day4.js
@@ -5,41 +5,63 @@ fs.readFile("Day4.txt", "utf8", function (err, data) {
   }
   let lines = data.split("\n");
 
-  let rollCount = 0;
+  const removedRolls = [];
+  let moreLeft = true;
+  let totalRowCount = 0;
 
-  lines.forEach((line, yI) => {
-    console.log(line.length);
-    const rollSlots = line.split("");
-    const yStart = yI - 1;
-    const yEnd = yI + 1;
+  while (moreLeft) {
+    let rollCount = 0;
+    lines.forEach((line, yI) => {
+      const rollSlots = line.split("");
+      const yStart = yI - 1;
+      const yEnd = yI + 1;
 
-    rollSlots.forEach((slot, ind) => {
-      if (slot !== "@") return;
-      let rollTots = 0;
+      rollSlots.forEach((slot, ind) => {
+        if (slot !== "@") return;
+        let rollTots = 0;
 
-      const xStart = ind - 1;
-      const xEnd = ind + 1;
+        const xStart = ind - 1;
+        const xEnd = ind + 1;
 
-      for (y = yStart; y <= yEnd; y++) {
-        if (y < 0 || y > lines.length - 1) continue;
-        let row = "";
-        for (x = xStart; x <= xEnd; x++) {
-          if (x < 0 || x > lines.length - 1) continue;
+        for (y = yStart; y <= yEnd; y++) {
+          if (y < 0 || y > lines.length - 1) continue;
+          let row = "";
+          for (x = xStart; x <= xEnd; x++) {
+            if (x < 0 || x > lines.length - 1) continue;
 
-          let slotToCheck = lines[y][x];
-          if (slotToCheck === "@") {
-            rollTots++;
+            let slotToCheck = lines[y][x];
+            if (slotToCheck === "@") {
+              rollTots++;
+            }
+            row += slotToCheck;
           }
-          row += slotToCheck;
         }
-        // console.log(row);
-      }
 
-      rollTots--; // minus 1 for the middle one we aren't counting
+        rollTots--; // minus 1 for the middle one we aren't counting
 
-      if (rollTots < 4) rollCount++;
+        if (rollTots < 4) {
+          removedRolls.push([yI, ind]);
+          rollCount++;
+        }
+      });
     });
-  });
 
-  console.log({ rollCount });
+    if (rollCount === 0) {
+      moreLeft = false;
+    } else {
+      // rebuild lines with removed rolls
+      removedRolls.forEach((coord) => {
+        const y = coord[0];
+        const x = coord[1];
+        let lineArr = lines[y].split("");
+        lineArr[x] = ".";
+        lines[y] = lineArr.join("");
+      });
+      totalRowCount += rollCount;
+      removedRolls.length = 0;
+      rollCount = 0;
+    }
+  }
+
+  console.log({ totalRowCount });
 });

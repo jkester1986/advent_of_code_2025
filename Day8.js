85b0cb27d8fd7ed7f1c3d19505bcd49b35534e48
Julia Kester <kester@oswego.edu>
2025-12-10 23:57:46 -0500

    Day 8 - got top 10 circuits sorted correctly

--- Day8.js
+++ Day8.js
@@ -10,23 +10,16 @@ fs.readFile("Day8.txt", "utf8", function (err, data) {
   lines.forEach((line, i) => {
     let currLine = line;
 
-    let lowestDistance = { distance: Infinity, point: null };
-
     lines.forEach((lineToCompare, ind) => {
       if (i === ind) return; // skip self
       const distance = getDistance(currLine, lineToCompare);
-      if (distance < lowestDistance.distance) {
-        lowestDistance = { distance, point: lineToCompare };
-      }
+      lineConnections[distance] = [
+        [currLine[0], currLine[1], currLine[2]],
+        lineToCompare,
+      ];
     });
-
-    lineConnections[lowestDistance.distance] = [
-      [currLine[0], currLine[1], currLine[2]],
-      lowestDistance.point,
-    ];
   });
 
-  console.log("lineConnections:", lineConnections);
   let shortestConnections = Object.keys(lineConnections)
     .map((key) => ({
       distance: Number(key),
@@ -35,80 +28,125 @@ fs.readFile("Day8.txt", "utf8", function (err, data) {
     .sort((a, b) => a.distance - b.distance)
     .slice(0, 10);
 
+  console.log("shortestConnections:", shortestConnections);
+
   const circuits = [];
   const visited = {};
 
-  // go through the connections and find the circuits and their lengths
-  shortestConnections.forEach(({ distance, connections }) => {
-    console.log("circtuits", circuits);
+  // // go through the connections and find the circuits and their lengths
+  // shortestConnections.forEach(({ distance, connections }) => {
+  //   console.log("circuits", circuits);
+  //   const [point1, point2] = connections;
+
+  //   // see if the current point is already connected to something else
+  //   const alreadyConnected = visited[`${point1[0]},${point1[1]},${point1[2]}`];
+  //   // see if the point it's connected to is already visited
+  //   const connnectionAlreadyVisited =
+  //     visited[`${point2[0]},${point2[1]},${point2[2]}`];
+
+  //   if (!alreadyConnected && !connnectionAlreadyVisited) {
+  //     console.log("start a new circuit for", point1);
+  //     // start a new circuit
+  //     circuits.push([point1, point2]);
+
+  //     // add both points to the visited list
+  //     visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
+  //       circuit: circuits.length - 1,
+  //     };
+  //     visited[`${point2[0]},${point2[1]},${point2[2]}`] = {
+  //       circuit: circuits.length - 1,
+  //     };
+  //   }
+  //   // else it's got a circuit connection already
+  //   else {
+  //     // it isn't connected yet, so connect it to the existing circuit
+  //     if (!alreadyConnected) {
+  //       console.log("adding this to existing circuit:", point1);
+  //       const circuitIndex = connnectionAlreadyVisited.circuit;
+  //       circuits[circuitIndex].push(point1);
+  //       visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
+  //         circuit: circuitIndex,
+  //       };
+  //     }
+  //     // it's already connected, let's get the new point it's connected to added to its circuit
+  //     else {
+  //       // check first to see if the new point has its own connection.
+
+  //       // If not, we can add the new point directly.
+  //       if (!connnectionAlreadyVisited) {
+  //         console.log(
+  //           "adding connected to something, which doesn't yet have a connection:",
+  //           point1
+  //         );
+  //         const circuitIndex = alreadyConnected.circuit;
+  //         circuits[circuitIndex].push(point1);
+  //         visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
+  //           circuit: circuitIndex,
+  //         };
+  //       }
+  //       // If so, we need to merge the circulits, and update all the visited points with the new circuit index
+  //       else {
+  //         console.log(
+  //           "both this point and it's connection already have other connections",
+  //           point1
+  //         );
+  //         const circuitIndex1 = alreadyConnected.circuit;
+  //         const circuitIndex2 = connnectionAlreadyVisited.circuit;
+
+  //         // merge circuit2 into circuit1
+  //         circuits[circuitIndex2].forEach((pt) => {
+  //           circuits[circuitIndex1].push(pt);
+  //           visited[pt] = {
+  //             circuit: circuitIndex1,
+  //           };
+  //         });
+  //         // clear out circuit2
+  //         circuits[circuitIndex2] = [];
+  //       }
+  //     }
+  //   }
+  // });
+
+  shortestConnections.forEach(({ connections }) => {
     const [point1, point2] = connections;
+    const stringP1 = point1.join(",");
+    const stringP2 = point2.join(",");
+    console.log("\n", { stringP1, stringP2, circuits });
 
-    // see if the current point is already connected to something else
-    const alreadyConnected = visited[`${point1[0]},${point1[1]},${point1[2]}`];
-    // see if the point it's connected to is already visited
-    const connnectionAlreadyVisited =
-      visited[`${point2[0]},${point2[1]},${point2[2]}`];
-
-    if (!alreadyConnected && !connnectionAlreadyVisited) {
-      console.log("start a new circuit for", point1);
-      // start a new circuit
-      circuits.push([point1, point2]);
-
-      // add both points to the visited list
-      visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
-        circuit: circuits.length - 1,
-      };
-      visited[`${point2[0]},${point2[1]},${point2[2]}`] = {
-        circuit: circuits.length - 1,
-      };
+    const addToCircuits = [];
+    loop: for (let i = 0; i < circuits.length; i++) {
+      const circuit = circuits[i];
+      console.log({ circuit });
+      if (circuit.includes(stringP1)) {
+        addToCircuits.push({ circuitIndex: i, point: stringP2 });
+      } else if (circuit.includes(stringP2)) {
+        addToCircuits.push({ circuitIndex: i, point: stringP1 });
+      }
+      if (addToCircuits.length === 2) break;
     }
-    // else it's got a circuit connection already
-    else {
-      // it isn't connected yet, so connect it to the existing circuit
-      if (!alreadyConnected) {
-        console.log("adding this to existing circuit:", point1);
-        const circuitIndex = connnectionAlreadyVisited.circuit;
-        circuits[circuitIndex].push(point1);
-        visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
-          circuit: circuitIndex,
-        };
-      }
-      // it's already connected, let's get the new point it's connected to added to its circuit
-      else {
-        // check first to see if the new point has its own connection.
 
-        // If not, we can add the new point directly.
-        if (!connnectionAlreadyVisited) {
-          console.log(
-            "adding connected to something, which doesn't yet have a connection:",
-            point1
-          );
-          const circuitIndex = alreadyConnected.circuit;
-          circuits[circuitIndex].push(point1);
-          visited[`${point1[0]},${point1[1]},${point1[2]}`] = {
-            circuit: circuitIndex,
-          };
-        }
-        // If so, we need to merge the circulits, and update all the visited points with the new circuit index
-        else {
-          console.log(
-            "both this point and it's connection already have other connections",
-            point1
-          );
-          const circuitIndex1 = alreadyConnected.circuit;
-          const circuitIndex2 = connnectionAlreadyVisited.circuit;
+    console.log({ addToCircuits });
 
-          // merge circuit2 into circuit1
-          circuits[circuitIndex2].forEach((pt) => {
-            circuits[circuitIndex1].push(pt);
-            visited[pt] = {
-              circuit: circuitIndex1,
-            };
-          });
-          // clear out circuit2
-          circuits[circuitIndex2] = [];
-        }
+    if (addToCircuits.length === 0) {
+      // start a new circuit
+      circuits.push([stringP1, stringP2]);
+    } else if (addToCircuits.length === 1) {
+      const p1 = addToCircuits[0];
+      circuits[p1.circuitIndex].push(p1.point);
+    } else {
+      if (addToCircuits[0].circuitIndex === addToCircuits[1].circuitIndex) {
+        // both points are already in the same circuit, do nothing
+        return;
       }
+      const p1 = addToCircuits[0];
+      const p2 = addToCircuits[1];
+      // connect them together
+      const combinedCircuit = [
+        ...circuits[p1.circuitIndex],
+        ...circuits[p2.circuitIndex],
+      ];
+      circuits[p1.circuitIndex] = combinedCircuit;
+      circuits[p2.circuitIndex] = [];
     }
   });
 
